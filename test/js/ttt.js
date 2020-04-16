67d546de1cc732c327613296935d07348e16a3dc
渣臻 <qz519189636@qq.com>
2020-04-16 18:58:57 +0800

    qz commit

--- test/js/ttt.js
+++ test/js/ttt.js
@@ -3,32 +3,19 @@
  * @return {number}
  */
 var findUnsortedSubarray = function (nums) {
-  let start = 0;
-  let end = nums.length - 1;
-  while (start + 1 < nums.length) {
-    if (nums[start] < nums[start + 1]) {
-      start++;
+  let l = nums.length - 1;
+  let r = 0;
+  let max = nums[i];
+  let min = nums[nums.length - 1];
+  for (let i = 1; i < nums.length; i++) {
+    if (nums[i] > max) {
+      max = nums[i];
     } else {
-      break;
+      
     }
   }
-  while (end - 1 >= 0) {
-    if (nums[end - 1] < nums[end]) {
-      end--;
-    } else {
-      break;
-    }
-  }
-  if (start >= end) {
-    // 原数组已经是一个升序数组
-    return 0;
-  }
-  console.log("start", start);
-  console.log("end", end);
-  debugger
-  return end - start + 1;
 };
 
-let param1 = [1,2,3,3,3];
+let param1 = [2, 6, 4, 8, 10, 9, 15];
 const res = findUnsortedSubarray(param1);
 console.log("res", res);
