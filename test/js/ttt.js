99a23e9014ef128d6d1569f275d235f393ddf995
渣臻 <qz519189636@qq.com>
2020-06-08 16:28:24 +0800

    qz commit

--- test/js/ttt.js
+++ test/js/ttt.js
@@ -1,25 +1,51 @@
-// https://leetcode-cn.com/problems/word-break/solution/dan-ci-chai-fen-by-leetcode/
 /**
- * @param {string} s
- * @param {string[]} wordDict
- * @return {boolean}
+ * @param {number[]} nums
+ * @param {number} S
+ * @return {number}
  */
-var wordBreak = function (s, wordDict) {
-  let dp = new Array(s.length + 1).fill(false);
-  dp[0] = true;
+var findTargetSumWays = function (nums, S) {
+  let sum = 0;
+  for (let i = 0; i < nums.length; i++) {
+    sum += nums[i];
+  }
 
-  for (let i = 1; i <= s.length; i++) {
-    for (let j = 0; j < i; j++) {
-      if (dp[j] && wordDict.indexOf(s.substring(j, i)) > -1) {
-        dp[i] = true;
-        break;
-      }
+  // 目标值的绝对值 超过 所有和的绝对值 无法达到
+  if (Math.abs(S) > Math.abs(sum)) {
+    return 0;
+  }
+
+  let len = nums.length;
+  let t = sum * 2 + 1;
+  let dp = new Array(len);
+
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(t).fill(0);
+  }
+
+  // 初始化
+  if (nums[0] == 0) {
+    dp[0][sum] = 2;
+  } else {
+    dp[0][sum + nums[0]] = 1;
+    dp[0][sum - nums[0]] = 1;
+  }
+
+  for (let i = 1; i < len; i++) {
+    for (let j = 0; j < t; j++) {
+      // 注意判断是否越界
+      let L = j - nums[i] >= 0 ? j - nums[i] : 0;
+      let R = j + nums[i] < t ? j + nums[i] : 0;
+      dp[i][j] = dp[i - 1][L] + dp[i - 1][R];
     }
   }
-  return dp[s.length]
+
+  console.log(dp);
+
+  // 结束的目标格子在 sum + S  (画出dp二维图可以知道)
+  return dp[len - 1][sum + S];
 };
 
-let s = "catsandog",
-  wordDict = ["cats", "dog", "sand", "and", "cat"];
-const res = wordBreak(s, wordDict);
-console.log("res111", res);
+const nums = [1, 1, 1, 1, 1];
+const S = 3;
+let res = findTargetSumWays(nums, S);
+console.log("res", res);
