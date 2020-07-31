f316c4e8392910c40d8eda4f832148c0a80645af
渣臻 <qz519189636@qq.com>
2020-07-31 18:32:36 +0800

    qz commit

--- test/js/ttt4.js
+++ test/js/ttt4.js
@@ -1,26 +1,31 @@
 /**
- * @param {number[]} numbers
+ * @param {number[]} nums
  * @return {number}
  */
-var minArray = function (numbers) {
-  let left = 0;
-  let right = numbers.length - 1;
-  while (left < right) {
-    let pivot = left + ~~((right - left) / 2);
-    if (numbers[pivot] < numbers[right]) {
-      right = pivot;
-    } else if (numbers[pivot] > numbers[right]) {
-      left = pivot + 1;
-    } else {
-      right -= 1;
-    }
-  }
-  return numbers[left];
+var findMagicIndex = function (nums) {
+  return getAnswer(nums, 0, nums.length - 1);
 };
 
-const parma1 = [3,1];
+function getAnswer(nums, left, right) {
+ if(left > right){
+   return -1 
+ }
+
+  let middle = left + parseInt((right - left) / 2);
+  let leftAnswer  = getAnswer(nums, left, middle - 1)
+  if(leftAnswer !== -1){
+    // 在左边找到了答案
+    return leftAnswer
+  }else if(nums[middle] === middle){
+    // 中间是答案
+    return middle
+  }
+  return getAnswer(nums, middle + 1, right);
+}
+
+const parma1 = [0, 2, 3, 4, 5]
 const parma2 = 1;
 const parma3 = 3;
 
-const res = minArray(parma1, parma2, parma3);
+const res = findMagicIndex(parma1, parma2, parma3);
 console.log("res", res);
