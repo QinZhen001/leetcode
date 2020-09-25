dfb8c70c0544896765af862d9a912f13ea93f202
渣臻 <qz519189636@qq.com>
2020-09-25 09:57:21 +0800

    qz commit

--- test/js/ttt4.js
+++ test/js/ttt4.js
@@ -1,7 +1,28 @@
 /**
  * @param {number[]} nums
- * @return {number[][]}
+ * @return {number}
  */
-var permuteUnique = function(nums) {
-
+var maxSubArray = function (nums) {
+  let dp = new Array(nums.length).fill(0);
+  dp[0] = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    if (dp[i - 1] >= 0) {
+      // 对当前dp[i]产生正影响 要
+      dp[i] = dp[i - 1] + nums[i];
+    } else {
+      // 对当前dp[i]产生负影响 不要
+      dp[i] = nums[i];
+    }
+  }
+  let max = dp[0];
+  for (let i = 1; i < dp.length; i++) {
+    if (dp[i] > max) {
+      max = dp[i]
+    }
+  }
+  return max
 };
+
+let nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4];
+const res = maxSubArray(nums);
+console.log("res", res);
