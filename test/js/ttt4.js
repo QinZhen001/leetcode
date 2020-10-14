4cb8fdf0c656630c6a971b9a862ec1c335208aad
渣臻 <qz519189636@qq.com>
2020-10-14 18:48:37 +0800

    qz commit

--- test/js/ttt4.js
+++ test/js/ttt4.js
@@ -1,39 +1,15 @@
 /**
- * @param {string} s
- * @return {number}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var lengthOfLongestSubstring = function (s) {
-  if (!s) {
-    return 0;
-  }
-  let n = s.length;
-  let set = new Set();
-  let right = 0;
-  let ans = 0;
-  for (let left = 0; left < n; left++) {
-     if(left!=0){
-      set.delete(s[left-1])
-     }
-    while (right < n && !set.has(s[right])) {
-      set.add(s[right])
-      right++;
-    }
-    ans = Math.max(ans,right-left)
-  }
-  return ans
+/**
+ * @param {ListNode} node
+ * @return {void} Do not return anything, modify node in-place instead.
+ */
+var deleteNode = function (node) {
+  node.next = node.next.next;
+  node = node.next;
 };
-
-const str = "abcabcbb";
-const res = lengthOfLongestSubstring(str);
-console.log("res", res);
-
-let set = new Set(); // 激励
-let left = 0; // 左指针
-let right = 0; // 右指针
-let ans = 0;
-for (let left = 0; left < n; left++) {
-  if (left != 0) {
-    set.delete(s[left - 1]);
-  }
-  while()
-}
