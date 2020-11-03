45fd1b5a2bfce520d1d324b973d78fcb613550b7
渣臻 <qz519189636@qq.com>
2020-11-03 17:59:40 +0800

    qz commit

--- test/js/ttt4.js
+++ test/js/ttt4.js
@@ -1,29 +1,43 @@
-function swap(arr,i,j){
-  [arr[i],arr[j]] = [arr[j],arr[i]] 
-}
-
-
 /**
- * @param {number[]} arr
- * @param {number} k
- * @return {number[]}
+ * @param {number[]} A
+ * @return {boolean}
  */
-var smallestK = function(arr, k) {
-  for(let i=0;i<k;i++){
-    for(let j=i+1;j<arr.length;j++){
-      if(arr[i]>arr[j]){
-        swap(arr,i,j)
-      }
+var validMountainArray = function (A) {
+  if (!A || !A.length || A.length < 3) {
+    return false
+  }
+  let maxIndex = 0
+  let maxValue = A[0]
+  for (let i = 0; i < A.length; i++) {
+    if (A[i] > maxValue) {
+      maxValue = A[i]
+      maxIndex = i
     }
   }
-  return arr.slice(0,k)
-};
+  // console.log(maxIndex,maxValue)
 
-let  arr = [1,3,5,7,2,4,6,8], k = 4
-const res =  smallestK(arr,k)
-console.log("res",res)
+  // 最大值在数组最后 或 最开始 
+  // 这种情况不是山脉
+  if(maxIndex == 0 || maxIndex == A.length-1){
+    return false
+  }
 
+  // 上升阶段
+  for (let i = 0; i < maxIndex; i++) {
+    if (A[i] >= A[i + 1]) {
+      return false
+    }
+  }
+  // 下降阶段
+  for (let i = maxIndex; i < A.length - 1; i++) {
+    if (A[i] <= A[i + 1]) {
+      return false
+    }
+  }
 
-// let arr = [1,2,3,4,5]
-// swap(arr,2,3)
-// console.log("arr",arr)
+  return true
+}
+
+let arr = [3, 5, 5]
+const res = validMountainArray(arr)
+console.log('res', res)
