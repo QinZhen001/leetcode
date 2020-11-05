9eed8bf00f36fe2620b140136cb89aaec7c0e90a
渣臻 <qz519189636@qq.com>
2020-11-05 18:19:39 +0800

    Update ttt4.js

--- test/js/ttt4.js
+++ test/js/ttt4.js
@@ -19,7 +19,7 @@ var WordsFrequency = function (book) {
  * @return {number}
  */
 WordsFrequency.prototype.get = function (word) {
-  
+  return this.map.get(word) || 0
 }
 
 let wordsFrequency = new WordsFrequency([
@@ -34,11 +34,15 @@ let wordsFrequency = new WordsFrequency([
 ])
 console.log(wordsFrequency)
 
-// wordsFrequency.get('you') //返回0，"you"没有出现过
-// wordsFrequency.get('have') //返回2，"have"出现2次
-// wordsFrequency.get('an') //返回1
-// wordsFrequency.get('apple') //返回1
-// wordsFrequency.get('pen') //返回1
+let a1 = wordsFrequency.get('you') //返回0，"you"没有出现过
+let a2 = wordsFrequency.get('have') //返回2，"have"出现2次
+let a3 = wordsFrequency.get('an') //返回1
+let a4 = wordsFrequency.get('apple') //返回1
+let a5 = wordsFrequency.get('pen') //返回1
+
+console.log(a1)
+
+// console.log(a1,a2,a3,a4,a5)
 
 /**
  * Your WordsFrequency object will be instantiated and called as such:
