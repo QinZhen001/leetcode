4568717a5ef7b9d2a52bf40863da964875b5462b
qz <qz519189636@qq.com>
2020-11-07 16:42:58 +0800

    qz commit

--- test/js/ttt.js
+++ test/js/ttt.js
@@ -4,48 +4,48 @@
  * @return {number}
  */
 var findTargetSumWays = function (nums, S) {
-  let sum = 0;
+  let sum = 0
   for (let i = 0; i < nums.length; i++) {
-    sum += nums[i];
+    sum += nums[i]
   }
 
   // 目标值的绝对值 超过 所有和的绝对值 无法达到
   if (Math.abs(S) > Math.abs(sum)) {
-    return 0;
+    return 0
   }
 
-  let len = nums.length;
-  let t = sum * 2 + 1;
-  let dp = new Array(len);
+  let len = nums.length
+  let t = sum * 2 + 1
+  let dp = new Array(len)
 
   for (let i = 0; i < dp.length; i++) {
-    dp[i] = new Array(t).fill(0);
+    dp[i] = new Array(t).fill(0)
   }
 
   // 初始化
   if (nums[0] == 0) {
-    dp[0][sum] = 2;
+    dp[0][sum] = 2
   } else {
-    dp[0][sum + nums[0]] = 1;
-    dp[0][sum - nums[0]] = 1;
+    dp[0][sum + nums[0]] = 1
+    dp[0][sum - nums[0]] = 1
   }
 
   for (let i = 1; i < len; i++) {
     for (let j = 0; j < t; j++) {
       // 注意判断是否越界
-      let L = j - nums[i] >= 0 ? j - nums[i] : 0;
-      let R = j + nums[i] < t ? j + nums[i] : 0;
-      dp[i][j] = dp[i - 1][L] + dp[i - 1][R];
+      let L = j - nums[i] >= 0 ? j - nums[i] : 0
+      let R = j + nums[i] < t ? j + nums[i] : 0
+      dp[i][j] = dp[i - 1][L] + dp[i - 1][R]
     }
   }
 
-  console.log(dp);
+  console.log(dp)
 
   // 结束的目标格子在 sum + S  (画出dp二维图可以知道)
-  return dp[len - 1][sum + S];
-};
+  return dp[len - 1][sum + S]
+}
 
-const nums = [1, 1, 1, 1, 1];
-const S = 3;
-let res = findTargetSumWays(nums, S);
-console.log("res", res);
+const nums = [1, 1, 1, 1, 1]
+const S = 3
+let res = findTargetSumWays(nums, S)
+console.log('res', res)
