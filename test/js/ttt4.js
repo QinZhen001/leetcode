bac344dd252e1beb11178c513c5feb3710e50605
渣臻 <qz519189636@qq.com>
2020-11-19 18:43:11 +0800

    qz commit

--- test/js/ttt4.js
+++ test/js/ttt4.js
@@ -1,29 +1,40 @@
 /**
- * @param {number[]} nums
- * @return {number}
+ * @param {number[]} digits
+ * @return {number[]}
  */
-var findPeakElement = function (nums) {
-  return binarySearch(nums, 0, nums.length - 1)
+var plusOne = function (digits) {
+  for (let i = digits.length - 1; i >= 0; i--) {
+    if (i == 0) {
+      // 到了第一位
+      if (digits.length == 1) {
+        // 只有一位数
+        digits[i]++
+      }
+      if (digits[i] == 10) {
+        // 进位
+        digits[i] = 0
+        digits.unshift(1)
+        break
+      }
+    } else if (i == digits.length - 1) {
+      // 到了最后一位
+      digits[i]++
+      if (digits[i] == 10) {
+        // 进位
+        digits[i] = 0
+        digits[i - 1] = digits[i - 1] + 1
+      }
+    } else {
+      if (digits[i] == 10) {
+        // 进位
+        digits[i] = 0
+        digits[i - 1] = digits[i - 1] + 1
+      }
+    }
+  }
+  return digits
 }
 
-function binarySearch(nums, l, r) {
-  let mid = Math.round((l + r) / 2)
-  if (mid == l || mid == r) {
-    // 递归终止的标志
-    // 峰值肯定在 l 或 r 中的较大的那个
-    return nums[l] > nums[r] ? l : r
-  }
-  if (nums[mid] > nums[mid + 1]) {
-    // mid 处于局部下降序列
-    // 峰值在 mid 左边
-    return binarySearch(nums, l, mid)
-  } else {
-    return binarySearch(nums, mid, r)
-  }
-}
-
-// [2,1]
-
-const params = [2, 1]
-const res = findPeakElement(params)
-console.log(res)
+const nums = [9]
+const res = plusOne(nums)
+console.log('res', res)
