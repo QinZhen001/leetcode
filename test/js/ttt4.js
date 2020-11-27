489c25c116fef865a733e38167c7e7785942f97c
渣臻 <qz519189636@qq.com>
2020-11-27 18:36:26 +0800

    qz commit

--- test/js/ttt4.js
+++ test/js/ttt4.js
@@ -1,40 +1,48 @@
 /**
- * @param {number[]} digits
- * @return {number[]}
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
  */
-var plusOne = function (digits) {
-  for (let i = digits.length - 1; i >= 0; i--) {
-    if (i == 0) {
-      // 到了第一位
-      if (digits.length == 1) {
-        // 只有一位数
-        digits[i]++
-      }
-      if (digits[i] == 10) {
-        // 进位
-        digits[i] = 0
-        digits.unshift(1)
-        break
-      }
-    } else if (i == digits.length - 1) {
-      // 到了最后一位
-      digits[i]++
-      if (digits[i] == 10) {
-        // 进位
-        digits[i] = 0
-        digits[i - 1] = digits[i - 1] + 1
-      }
-    } else {
-      if (digits[i] == 10) {
-        // 进位
-        digits[i] = 0
-        digits[i - 1] = digits[i - 1] + 1
-      }
+var merge = function (nums1, m, nums2, n) {
+  let zeroLen = nums1.length - m
+  nums1.splice(m, zeroLen)
+  // debugger
+  let copyNums1 = nums1.slice()
+  let i = 0
+  let j = 0
+  let index = 0
+  while (i < copyNums1.length || j < nums2.length) {
+    if (i >= copyNums1.length) {
+      nums1[index++] = nums2[j++]
+      continue
     }
+
+    if (j >= nums2.length) {
+      nums1[index++] = copyNums1[i++]
+      continue
+    }
+
+    if (copyNums1[i] < nums2[j]) {
+      nums1[index++] = copyNums1[i++]
+    } else {
+      nums1[index++] = nums2[j++]
+    }
+    // index ++
   }
-  return digits
+  console.log(nums1)
 }
 
-const nums = [9]
-const res = plusOne(nums)
+let nums1 = [1]
+let m = 1
+let nums2 = []
+let n = 0
+const res = merge(nums1, m, nums2, n)
 console.log('res', res)
+
+// ----------
+// let aaa = []
+// let bbb = [1]
+// console.log(aaa[0])
+// console.log(aaa[0] < bbb[0])
