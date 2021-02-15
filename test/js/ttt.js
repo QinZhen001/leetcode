58ce6563835b3d6acb2481b116c72a4d9781f3d2
渣臻 <qz519189636@qq.com>
2021-02-15 23:41:22 +0800

    qz commit

--- test/js/ttt.js
+++ test/js/ttt.js
@@ -1,24 +1,3 @@
-/**
- * @param {number[]} nums
- * @param {number} k
- * @return {number}
- */
-var subarraySum = function (nums, k) {
-  // totals[i] 记录 0 到 i-1 元素的和
-  let totals = [] 
-  totals[0] = 0
-  for (let i = 1; i <= nums.length; i++) {
-    totals[i] = totals[i - 1] + nums[i - 1]
-  }
-
-  for (let start = 0; start < nums.length; start++) {
-    for (let end = start + 1; end <= nums.length; end++) {
-      // totals[end] - totals[start] => 可以知道 start到end位置的和
-      let num = totals[end] - totals[start]
-
-      // ....
-    }
-  }
-  
-  // ..
-}
+const arr = [1, 2, 3, 4, 5];
+const res = arr.slice(2, 10);
+console.log(res);
--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -1,39 +1,26 @@
-const paths = []
-
-function test(arr) {
-  arr.sort((a,b)=>b-a)
-  dfs(0, arr.length, [], [], arr);
-  console.log(paths)
-}
-
 /**
- * 
- * @param {*} start 
- * @param {*} end 
- * @param {*} visited 
- * @param {*} path 
- * @param {*} arr 
+ * @param {number[][]} matrix
+ * @return {void} Do not return anything, modify matrix in-place instead.
  */
-function dfs(start,end,visited,path,arr){
-  if(start == end){
-    paths.push(
-      path.slice()
-    )
-    return
+var rotate = function (matrix) {
+  const len = matrix.length;
+  const arr = new Array(matrix.length);
+  for (let i = 0; i < arr.length; i++) {
+    arr[i] = new Array(arr.length).fill(0);
   }
-
-  for(let i=0;i<arr.length;i++){
-    if(!visited[i]){
-      visited[i] = true 
-      path.push(arr[i])
-      dfs(start+1,end,visited,path,arr)
-      visited[i] = false
-      path.pop()
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix.length; j++) {
+      arr[j][len - i - 1] = matrix[i][j];
     }
   }
+  matrix = arr;
+  console.log(matrix);
+};
 
-}
-
-const arr = [1, 2, 3];
-const res = test(arr);
+const arr = [
+  [1, 2, 3],
+  [4, 5, 6],
+  [7, 8, 9],
+];
+const res = rotate(arr);
 console.log("res", res);
