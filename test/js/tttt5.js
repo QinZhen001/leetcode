e2fc8d609278cc83985829b83daa61c0989dfd21
渣臻 <qz519189636@qq.com>
2021-02-18 18:05:30 +0800

    qz commit

--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -4,29 +4,40 @@
  */
 var longestConsecutive = function (nums) {
   if (!nums || !nums.length) {
-    return 0;
+    return 0
   }
-  nums.sort((a, b) => a - b);
-  let max = 1;
-  let count = 1;
-  for (let i = 1; i < nums.length; i++) {
-    if (nums[i] - nums[i - 1] == 1) {
-      count++;
-    } else if (nums[i] == nums[i - 1]) {
-      // 前后元素相等
-      continue;
+  // 去重
+  let set = new Set(nums)
+  let cur = 1
+  let max = 1
+  for (let item of nums) { 
+    if (set.has(item - 1)) {
+      // 存在item-1
+      // item肯定不是起点
     } else {
-      max = Math.max(max, count);
-      count = 1;
+      cur = calcLongest(item, set)
+      max = Math.max(cur, max)
     }
   }
-  console.log(arr);
-  max = Math.max(max, count);
-  return max;
-};
+  return max 
+}
+
+/**
+ * 以start为起点计算
+ * @param {*} start
+ * @param {*} set
+ */
+function calcLongest(start, set) {
+  let num = 0
+  while (set.has(start)) {
+    start++
+    num++
+  }
+  return num
+}
 
 // const arr = [0, 3, 7, 2, 5, 8, 4, 6, 0, 1];
 // const arr = [100, 4, 200, 1, 3, 2];
-const arr = [1, 2, 0, 1];
-const res = longestConsecutive(arr);
-console.log("res", res);
+const arr = [1, 2, 0, 1]
+const res = longestConsecutive(arr)
+console.log('res', res)
