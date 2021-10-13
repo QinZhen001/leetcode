1f339d639dbf05c8e922e70347c9357aab0f2942
qinzhen <qz519189636@qq.com>
2021-10-13 20:22:55 +0800

    qz commit

--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -1,43 +1,10 @@
-/**
- * @param {string} a
- * @param {string} b
- * @return {string}
- */
-var addBinary = function (a, b) {
-  let len1 = a.length;
-  let len2 = b.length;
-  if (len1 < len2) {
-    a = a.padStart(len2, "0");
-  } else {
-    b = b.padStart(len1, "0");
-  }
 
-  a = a.split("");
-  b = b.split("");
+const path = `
+ adada
+ asdadsa
+ asdasda
+ asdasd
+`;
 
-  let carry = 0;
-  let arr = [];
-  for (let i = a.length - 1; i >= 0; i--) {
-    let num = +a[i] + +b[i] + carry;
-    if (num >= 2) {
-      num = num % 2;
-      carry = 1;
-    } else {
-      carry = 0;
-    }
-    arr[i] = num;
-  }
-  if (carry) {
-    arr.unshift(1);
-  }
-  return arr.join("");
-};
-
-// const a = "1010";
-// const b = "1011";
-
-const a = "11";
-const b = "1";
-
-const ddd = addBinary(a, b);
-console.log("dddd", ddd);
+const res = simplifyPath(path);
+console.log("res", res);
