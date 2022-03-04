2c1fd3885f59b3e772a2681c6db6960e3b174164
qinzhen <qz519189636@qq.com>
2022-03-04 22:03:08 +0800

    qz commit

---
+++ test/asd.vue
@@ -0,0 +1,38 @@
+ <template>
+<div>
+<p>a: {{ a }}</p >
+<p>b: {{ b }}</p >
+<p>a + b: {{ plus(a, b) }}</p >
+<p>c: {{ c }}</p >
+<p>d: {{ d }}</p >
+<a-button type="primary" @click="() => { c = c + 1 }">c+1</a-button>
+</div>
+</template>
+
+<script>
+export default {
+name: 'Test',
+
+data () {
+return {
+a: 1,
+b: 1,
+c: 1
+}
+},
+
+computed: {
+d () {
+console.log('I am d.')
+return this.a + this.b
+}
+},
+
+methods: {
+plus (a, b) {
+console.log('I am a plus b.')
+return this.a + this.b
+}
+}
+}
+</script>
