037657dce5008e1ed0383bdfb7b5b1f95ba17704
qinzhen <qz519189636@qq.com>
2022-03-08 19:09:27 +0800

    qz commit

--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -1,19 +1,37 @@
 /**
- * @param {number[]} nums
- * @return {string}
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
  */
-var minNumber = function (nums) {
-  nums.sort((a, b) => {
-    //x+y>y+x
-    let num1 = '' + a + b
-    let num2 = '' + b + a
-    return num1 - num2
-  })
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+  if (!l1 && !l2) {
+    return null;
+  }
+  let temp = {};
+  let head = temp;
+  let node1 = l1;
+  let node2 = l2;
+  while (node1 || node2) {
+    if(!node1){
 
-  return nums.join('')
-}
+    }else 
+    if (node1.val < node2.val) {
+      temp.next = node1;
+      node1 = node1.next;
+      temp = temp.next;
+    } else {
+      temp.next = node2;
+      node2 = node2.next;
+      temp = temp.next;
+    }
+  }
 
-let nums = [3, 30, 34, 5, 9]
-const res = minNumber(nums)
-
-console.log('res', res)
+  return head.next;
+};
