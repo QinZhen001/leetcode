fb5062f6c4397f5d5791647c98733becdf5a4cb0
qz <qz519189636@qq.com>
2022-03-18 23:31:04 +0800

    qz commit

--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -1,37 +1,62 @@
 /**
- * Definition for singly-linked list.
- * function ListNode(val) {
- *     this.val = val;
- *     this.next = null;
- * }
+ * @param {string} s
+ * @return {boolean}
  */
-/**
- * @param {ListNode} l1
- * @param {ListNode} l2
- * @return {ListNode}
- */
-var mergeTwoLists = function (l1, l2) {
-  if (!l1 && !l2) {
-    return null;
+var checkValidString = function (s) {
+  if (s == '') {
+    return true
   }
-  let temp = {};
-  let head = temp;
-  let node1 = l1;
-  let node2 = l2;
-  while (node1 || node2) {
-    if(!node1){
 
-    }else 
-    if (node1.val < node2.val) {
-      temp.next = node1;
-      node1 = node1.next;
-      temp = temp.next;
-    } else {
-      temp.next = node2;
-      node2 = node2.next;
-      temp = temp.next;
+  if (s.length == 1) {
+    return s[0] == '*'
+  }
+
+  if (s.length == 2) {
+    return s == '()' || s == '(*' || s == '*)' || s == '**'
+  }
+
+  // dp[i][j] =>  字符串i-j
+  let dp = new Array(s.length)
+  for (let i = 0; i < dp.length; i++) {
+    dp[i] = new Array(s.length).fill(false)
+  }
+  // dp 初始化
+
+  // 一位的情况
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] == '*') {
+      dp[i][i] = true
     }
   }
 
-  return head.next;
-};
+  // 两位的情况
+  for (let i = 1; i < s.length; i++) {
+    const c1 = s[i - 1]
+    const c2 = s[i]
+    dp[i - 1][i] = (c1 === '(' || c1 === '*') && (c2 === ')' || c2 === '*')
+  }
+
+  // 从后往前 最少长度3
+  for (let i = s.length - 3; i >= 0; i--) {
+    const c1 = s[i]
+    for (let j = i + 2; j < s.length; j++) {
+      const c2 = s[j]
+      if ((c1 === '(' || c1 === '*') && (c2 === ')' || c2 === '*')) {
+        dp[i][j] = dp[i + 1][j - 1]
+      }
+      // 存在k 将dp[i][j]分割成两个子字符串
+      // 如果两个子字符串满足规则 那么dp[i][j] 也满足规则
+      for (let k = i; k < j; k++) {
+        if (dp[i][k] && dp[k + 1][j]) {
+          dp[i][j] = true
+          break
+        }
+      }
+    }
+  }
+  return dp[0][s.length - 1]
+}
+
+let str = '(*'
+const res = checkValidString(str)
+console.log('res', res)
