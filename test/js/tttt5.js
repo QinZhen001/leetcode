966858ab46fbc894c21a2163dfdef4c6dbc6086b
qz <qz519189636@qq.com>
2022-03-22 23:00:30 +0800

    qz commit

--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -1,34 +1,21 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
-var maxDepth = function (root) {
-  if (!root) {
-    return 0
-  }
+function adjust(array,target,len){
+  for(let i=2*target+1;i<len;)
+}
 
-  let stack = []
-  stack.push(root)
-  let total = 1
-
-  while (stack.length) {
-    const currentLevelSize = stack.length
-    for (let i = 0; i < currentLevelSize; i++) {
-      let node = stack.shift()
-      if (node.left) {
-        stack.push(node.left)
-      }
-      if (node.right) {
-        stack.push(node.right)
-      }
-    }
-    total++
+// 构建大顶堆
+function creatHeap(array) {
+  const len = array.length
+  const start = parseInt(len / 2) - 1
+  for (let i = start; i >= 0; i--) {
+      adjust(array,i,len)
   }
 }
+
+function heapSort(array) {
+  creatHeap(array)
+  console.log(array)
+}
+
+let nums = [3, 2, 1, 5, 6, 4]
+const res = mergeSort(nums)
+console.log('res', res)
