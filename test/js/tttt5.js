bc25041dd07afae1dda495e929600c01754e50b9
qinzhen <qz519189636@qq.com>
2022-04-16 23:45:15 +0800

    qz commit

--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -1,43 +1,12 @@
-class Timer {
-  constructor(num) {
-    this.num = num;
-    this.timer = null;
-    this.hasStarted = false;
-  }
-  start() {
-    if (this.hasStarted) {
-      return;
-    }
-    this.hasStarted = true;
-    if (this.timer) {
-      // 处理pause之后再start
-      clearInterval(this.timer);
-    }
-    this.timer = setInterval(() => {
-      if (this.num < 0 || !this.hasStarted) {
-        return;
-      }
-      console.log(this.num);
-      this.num--;
-    }, 1000);
-  }
-  pause() {
-    if (!this.hasStarted) {
-      return;
-    }
-    this.hasStarted = false;
-  }
-  cancel() {
-    if (!this.hasStarted) {
-      return;
-    }
-    this.hasStarted = false;
-    this.num = 0
-    if (this.timer) {
-      clearInterval(this.timer);
-      this.timer = null;
-    }
-  }
-}
+/**
+ * @param {number[]} coins
+ * @param {number} amount
+ * @return {number}
+ */
+var coinChange = function (coins, amount) {
+  
+};
 
-
+let coins = [1, 2, 5],
+  amount = 11;
+coinChangecoins(coins, amount);
