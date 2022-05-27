eff589730a9e426a8506da5f6462fdc6ac2daeca
qinzhen <qz519189636@qq.com>
2022-05-27 20:24:43 +0800

    qz commit

--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -1,43 +1,13 @@
-/**
- * @param {string} word1
- * @param {string} word2
- * @return {number}
- */
-var minDistance = function (word1, word2) {
-  let len1 = word1.length;
-  let len2 = word2.length;
-  let dp = new Array(len1);
-  for (let i = 0; i < dp.length; i++) {
-    dp[i] = new Array(len2).fill(0);
-  }
-  // dp[i][j] =>  word1[0, ...i] 变为 word2[0,...j] 所需要的步骤
+const delay = 2;
+const limit = 2;
+let i = 1;
 
-  // init
-  for (let i = 0; i < len1; i++) {
-    dp[i][0] = i;
-  }
-  for (let i = 0; i < len2.length; i++) {
-    dp[0][i] = i;
-  }
+console.log("START!");
+const limitedInterval = setInterval(() => {
+  console.log(`message ${i}, appeared after ${delay * i++} seconds`);
 
-  for (let i = 0; i < len1; i++) {
-    for (let j = i; j < len2; j++) {
-      if (word1[i] == word2[j]) {
-        if (i == 0 || j == 0) {
-          continue;
-        } else {
-          dp[i][j] = dp[i - 1][j - 1];
-        }
-      } else {
-        dp[i][j] = 1 + Math.min(dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]);
-      }
-    }
+  if (i > limit) {
+    clearInterval(limitedInterval);
+    console.log("interval cleared!");
   }
-
-  return dp[len1 - 1][len2 - 1];
-};
-
-let word1 = "horse";
-let word2 = "ros";
-const res = minDistance(word1, word2);
-console.log("res", res, map);
+}, delay * 1000);
