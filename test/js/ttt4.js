46f1a922dd49f47d89caadb77c6fb8f581e560dd
qinzhen <qz519189636@qq.com>
2023-02-19 00:09:24 +0800

    featL qz

--- test/js/ttt4.js
+++ test/js/ttt4.js
@@ -1,13 +1,39 @@
+// public class Solution {
+//   public int maxProfit(int prices[]) {
+//       int minprice = Integer.MAX_VALUE;
+//       int maxprofit = 0;
+//       for (int i = 0; i < prices.length; i++) {
+//           if (prices[i] < minprice) {
+//               minprice = prices[i];
+//           } else if (prices[i] - minprice > maxprofit) {
+//               maxprofit = prices[i] - minprice;
+//           }
+//       }
+//       return maxprofit;
+//   }
+// }
+
+
+
 /**
- * @param {string} word1
- * @param {string} word2
+ * @param {number[]} prices
  * @return {number}
  */
-var minDistance = function (word1, word2) {
-  
+var maxProfit = function (prices) {
+  let minPrice = Number.MAX_VALUE
+  let maxProfit = 0
+  for (let i = 0;i < prices.length;i++) {
+    if (prices[i] < minPrice) {
+      minPrice = prices[i]
+    } else if (prices[i] - minPrice > maxProfit) {
+      maxProfit = prices[i] - minPrice
+    }
+  }
+
+  return maxProfit
 };
 
-let word1 = "horse";
-let word2 = "ros";
-const res = minDistance(word1, word2);
-console.log("res", res);
+
+const arr = [7,1,5,3,6,4]
+const res = maxProfit(arr)
+console.log(res)
--- test/js/tttt5.js
+++ test/js/tttt5.js
@@ -1,13 +1,73 @@
-const delay = 2;
-const limit = 2;
-let i = 1;
+/**
+实现洗牌算法函数shuffle，给定一个数组[0,1,2,3,4,5,6]，每次随机抽选数组的n个值，连续抽选不重复已经抽选的值，直到数组抽完，再进行下一轮循环。
 
-console.log("START!");
-const limitedInterval = setInterval(() => {
-  console.log(`message ${i}, appeared after ${delay * i++} seconds`);
+示例1：
+var random = shuffle([0,1,2,3,4,5,6]);
+random(1); // [1]
+random(1); // [0]
+random(1); // [2]
+random(1); // [3]
+random(1); // [5]
+random(1); // [4]
+random(1); // [6]
+random(1); // [3]
 
-  if (i > limit) {
-    clearInterval(limitedInterval);
-    console.log("interval cleared!");
-  }
-}, delay * 1000);
+示例2：
+var random = shuffle([0,1,2,3,4,5,6]);
+random(1); // [1]
+random(2); // [0,6]
+random(1); // [2]
+random(4); // [3,4,5,2]
+**/
+
+
+
+// 二题
+var data = [
+  {
+      parentId: 0,
+      id: 1,
+      value: '1'
+  },
+  {
+      parentId: 3,
+      id: 2,
+      value: '2'
+  },
+  {
+      parentId: 0,
+      id: 3,
+      value: '3'
+  },
+  {
+      parentId: 1,
+      id: 4,
+      value: '4'
+  },
+  {
+      parentId: 1,
+      id: 5,
+      value: '5'
+  },
+];
+
+// 请完成toTree函数将该数据整理为树状结构, 该树每个节点的结构如下,
+// node: Node = {
+//     children: Node[],
+//     parentId,
+//     id,
+//     value,
+// }
+
+
+
+
+// 插件体制设计实现
+// 
+// electorn 视频渲染
+// 
+// 
+// 
+
+
+
